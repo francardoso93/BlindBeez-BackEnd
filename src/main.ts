8378b1632fb3eeb55ca46e72344c387ed9c55902
francardoso <francardoso@outlook.com>
2020-05-30 12:01:26 -0300

    Removendo ssl

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const fs = require('fs');
-  const certFile = fs.readFileSync(process.env.CERT_FILE_PATH ? process.env.CERT_FILE_PATH : '/home/francisco/ssl/localhost.crt');
-  const keyFile = fs.readFileSync(process.env.KEY_FILE_PATH ? process.env.KEY_FILE_PATH : '/home/francisco/ssl/localhost.key');
-
-  const app = await NestFactory.create(AppModule,
-    {
-      httpsOptions: {
-        key: keyFile,
-        cert: certFile,
-      },
-    },
-  );
+  const app = await NestFactory.create(AppModule);
 
   app.enableCors();
   await app.listen(3000);
